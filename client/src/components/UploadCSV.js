8b8cf8f1c907f2a55ef651b6b1ace2a903b23ecc
Francis Badasu <nytro04@gmail.com>
2020-02-04 00:21:58 +0000

    show details from pdf

--- client/src/components/UploadCSV.js
+++ client/src/components/UploadCSV.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 import axios from "axios";
+import slugify from "slugify";
 
 class UploadCSV extends Component {
   state = {
     file: "",
-    imagePreviewUrl: ""
+    imagePreviewUrl: "",
+    jsonData: []
+  };
+
+  renderTableData = () => {
+    return this.state.jsonData.map((report, index) => {
+      console.log(report);
+
+      // console.log(JSON.parse(report));
+      const { field1, field2, field3, field4, field5, field6 } = report;
+
+      // console.log("employee id", employeeId);
+      // console.log("company name", name);
+
+      return (
+        <tr key={index}>
+          <td>{field1}</td>
+          <td>{field2}</td>
+          <td>{field3}</td>
+          <td>{field4}</td>
+          <td>{field5}</td>
+          <td>{field6}</td>
+        </tr>
+      );
+    });
   };
 
   handleUpload = e => {
@@ -15,9 +40,12 @@ class UploadCSV extends Component {
     data.append("file", this.uploadInput.files[0]);
 
     axios
-      .post("http://localhost:8002/api/v1/upload", data)
+      .post("http://localhost:8080/api/v1/upload", data)
       .then(res => {
-        console.log(res.data);
+        console.log(res.data.data.csvData);
+
+        const jsonData = res.data.data.csvData;
+        this.setState({ jsonData });
       })
       .catch(error => {
         console.log(error);
@@ -45,6 +73,8 @@ class UploadCSV extends Component {
   };
 
   render() {
+    const { jsonData } = this.state;
+    console.log(jsonData);
     return (
       <div className="container py-5">
         <h3 className="text-center mb-5">CSV TO HTML</h3>
@@ -60,6 +90,26 @@ class UploadCSV extends Component {
           </div>
           <button className="btn btn-info">Upload</button>
         </form>
+
+        <h2>HTML REPORT</h2>
+        <div>
+          <table>
+            <tbody>{this.renderTableData()}</tbody>
+            {/* <tbody>
+              {jsonData.map((report, index) => (
+                <tr key={index}>
+                  <td>test this</td>
+                  <td>{report.employeeId}</td>
+                  <td>{report.billableRate}</td>
+                  <td>{report.name}</td>
+                  <td>{report.date}</td>
+                  <td>{report.timeIn}</td>
+                  <td>{report.timeOut}</td>
+                </tr>
+              ))}
+            </tbody> */}
+          </table>
+        </div>
         {/* <form onSubmit={this.handleSubmit}>
           <div className="custom-file">
             <input
--- controllers/csvController.js
+++ controllers/csvController.js
@@ -1,5 +1,7 @@
 // const fs = require("fs");
 
+// const slugify = require("slugify");
+
 // const multer = require("multer");
 const csv = require("csvtojson");
 
@@ -9,11 +11,32 @@ const catchAsync = require("./../utils/catchAsync");
 // const testCSV = "./testCSV.csv";
 
 exports.uploadCSV = catchAsync(async (req, res, next) => {
-  const jsonArray = await csv({}).fromFile(req.file.path);
+  const jsonArray = await csv({ noheader: true }).fromFile(req.file.path);
+
+  console.log(jsonArray);
+
+  // const slugArray = jsonArray.map(report => {
+  //   return Object.keys(report).map(item => {
+  //     item.replace(" ", "-");
+  //   });
+  // });
 
   // console.log("file", testCSV);
 
-  console.log(jsonArray);
+  // const jsonObj = JSON.parse(jsonArray);
+  // console.log(jsonObj);
+
+  // const jsonContent = JSON.stringify(jsonArray);
+  // console.log(jsonContent);
+
+  // fs.writeFile("data.json", jsonContent, "utf8", err => {
+  //   if (err) {
+  //     console.log("An error occured while writing json object");
+  //     return console.log(err);
+  //   }
+
+  //   console.log("Json file successfully written");
+  // });
 
   res.status(200).json({
     status: "success",
